50899dc0ad87d32c0255466ff3bb785d59e2ee19
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-12-09 13:57:15 +0900

    test: 구입금액이 천원단위면 통과하는 테스트 추가

--- src/test/java/lotto/PriceTest.java
+++ src/test/java/lotto/PriceTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.*;
+
 import lotto.domain.Price;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,9 +13,18 @@ public class PriceTest {
     @DisplayName("구입금액이 천원 단위가 아니면 예외 발생한다.")
     @ParameterizedTest
     @ValueSource(ints = {1500, 2001, 2010})
-    void createPriceByPurchaseUnit(int price) {
-        Assertions.assertThatThrownBy(() -> Price.from(price))
+    void createPriceByPurchaseUnit1(int price) {
+        assertThatThrownBy(() -> Price.from(price))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구입 가격은 1000원 단위어야 합니다.");
     }
+
+    @DisplayName("구입금액이 천원 단위면 통과한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1000, 2000, 10000})
+    void createPriceByPurchaseUnit2(int price) {
+        assertThatCode(() -> Price.from(price))
+                .doesNotThrowAnyException();
+    }
+
 }
