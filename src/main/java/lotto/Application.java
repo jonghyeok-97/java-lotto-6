c2ac904e32467b17d4446c837c99b454c1cb7d44
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-12-09 13:46:43 +0900

    feat: 출력메시지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new LottoController(new InputView()).run();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,8 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public final class InputView {
 
     public int readPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         validateNumber(input);
+        return Integer.parseInt(input);
     }
 
     private void validateNumber(String input) {
